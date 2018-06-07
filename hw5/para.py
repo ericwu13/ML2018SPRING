009cccaf0489b6a9dd3482d3f818daee8e305ffc
eric86103 <b04901074@ntu.edu.tw>
2018-06-07 11:50:20 +0800

    para revise

--- hw5/para.py
+++ hw5/para.py
@@ -1,6 +1,6 @@
 import os
 base_dir = os.path.dirname((os.path.realpath(__file__)))
-corpus_path = os.path.join(base_dir, 'mydict/corpus_3W_30padd.txt')
-cmap_path = os.path.join(base_dir, 'mydict/cmap_3W_30padd.pkl')
-w2v_path = os.path.join(base_dir, 'mydict/word2vec_3W_30padd.pkl')
-token_path = os.path.join(base_dir, 'mydict/token.pkl')
+corpus_path = os.path.join(base_dir, 'model/corpus_3W_30padd.txt')
+cmap_path = os.path.join(base_dir, 'model/cmap_3W_30padd.pkl')
+w2v_path = os.path.join(base_dir, 'model/word2vec_3W_30padd.pkl')
+token_path = os.path.join(base_dir, 'model/token.pkl')
